868183e01f2990842132ac7c3662f7f74122d674
Mmarvinn <cerkunik@gmail.com>
2022-08-10 07:35:49 +0300

    create next branch to include API in card

--- src/components/card.js
+++ src/components/card.js
@@ -1,40 +1,16 @@
 'use strict';
 
-// import {getTopRatedFilms} from '../services/api.js';
+/* import {getTopRatedFilms} from '../services/api.js';
 
-// console.log(getTopRatedFilms);
+getTopRatedFilms().then(result => {
 
-/* getTopRatedFilms().then(result => {
-    let newDiv = document.createElement('div');
-    let pFilmName = document.createElement('p');
-    let pFilmOverview = document.createElement('p');
-    let filmImg = document.createElement('img');
-
-    filmImg.className = 'poster';
-    newDiv.className = 'card';
-    pFilmName.className = 'title';
-    pFilmOverview.className = 'overview';
-
-    filmImg.src = `http://image.tmdb.org/t/p/original${result[7]}`;
-    pFilmName.textContent = result[0];
-    pFilmOverview.textContent = result[1];
-
-    charts.append(newDiv);
-    newDiv.append(filmImg);
-    newDiv.append(pFilmName);
-    newDiv.append(pFilmOverview);
-
-    filmImg.addEventListener('click', showDetailingInfoAboutFilm);
+    console.log(result);
+    // filmImg.src = `http://image.tmdb.org/t/p/original${result[7]}`;
+    // pFilmName.textContent = result[0];
+    // pFilmOverview.textContent = result[1];
 
 }); */
 
-/* function showDetailingInfoAboutFilm() {
-    history.pushState({page: 1}, "title 1", "?page=1");
-    let divAboutFilm = document.createElement('div');
-    let backGroundImage = 
-
-    divAboutFilm.style.zIndex = '1000';
-} */
 
 export function createMovieCard() {
 
--- src/services/api.js
+++ src/services/api.js
@@ -1,36 +1,38 @@
 'use strict';
 
-const baseUrl = 'https://api.themoviedb.org/3'; // to upper casse
-const baseImgUrl = 'http://image.tmdb.org/t/p/';
-const apiKey = '1be31873de9aa764d36bafbd53752fba';
+const BASEURL = 'https://api.themoviedb.org/3';
+const BASEIMGURL = 'http://image.tmdb.org/t/p/';
+const APIKEY = '1be31873de9aa764d36bafbd53752fba';
 
 export async function getTopRatedFilms() {
 
-    let response = await fetch(`${baseUrl}/movie/popular?api_key=${apiKey}&language=en-US&page=1`);
+    let response = await fetch(`${BASEURL}/movie/popular?api_key=${APIKEY}&language=en-US&page=1`);
     let json = await response.json();
 
     console.log(json);
 
     let propOfFilm = [];
-    let posterOfFilm = json.results[0].poster_path;
-    let filmName = json.results[0].original_title;
-    let filmOverview = json.results[0].overview;
-    let backdrop = json.results[0].backdrop_path;
-    let poster = json.results[0].poster_path; 
-    let releaseDate = json.results[0].release_date;
-    let voteAverage = json.results[0].vote_average;
-    let voteCount = json.results[0].vote_count;
 
-    propOfFilm.push(filmName);
-    propOfFilm.push(filmOverview);
-    propOfFilm.push(backdrop);
-    propOfFilm.push(poster);
-    propOfFilm.push(releaseDate);
-    propOfFilm.push(voteAverage);
-    propOfFilm.push(voteCount);
-    propOfFilm.push(posterOfFilm);
+    for (let i = 0; i < json.results.length; i++) {
+    
+        let posterOfFilm = json.results[i].poster_path;
+        let filmName = json.results[i].original_title;
+        let filmOverview = json.results[i].overview;
+        let backdrop = json.results[i].backdrop_path;
+        let poster = json.results[i].poster_path; 
+        let releaseDate = json.results[i].release_date;
+        let voteAverage = json.results[i].vote_average;
+        let voteCount = json.results[i].vote_count;
+
+        propOfFilm.push(filmName);
+        propOfFilm.push(filmOverview);
+        propOfFilm.push(backdrop);
+        propOfFilm.push(poster);
+        propOfFilm.push(releaseDate);
+        propOfFilm.push(voteAverage);
+        propOfFilm.push(voteCount);
+        propOfFilm.push(posterOfFilm);
+    }
 
     return propOfFilm;
 }
-
-// switch : case
