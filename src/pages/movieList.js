f2f2059bd418503caf113e2b902eae7f2a3f9bc6
Mmarvinn <cerkunik@gmail.com>
2022-08-11 13:53:13 +0300

    add display error in case this happens

--- src/pages/movieList.js
+++ src/pages/movieList.js
@@ -5,33 +5,36 @@ import {renderPageLayout} from '../layouts/createLayout.js';
 import {getTopRatedFilms} from '../services/api.js';
 
 export function createMovieList() {
+    getTopRatedFilms().then(onFullFilled, onRejected);  
+}
+
+function onFullFilled(result) {
     const arrayWithCards = [];
 
-    getTopRatedFilms().then(onFullFilled, onRejected);
-    
-    function onFullFilled(result) {
-        console.log(result);
-        if (result?.results) {
+    if (result?.results) {
 
-            result.results.forEach(element => {
-                const movieInfo = {};
+        result.results.forEach(element => {
+            const movieInfo = {};
 
-                movieInfo.posterOfFilm = element.poster_path;
-                movieInfo.filmName = element.original_title;
-                movieInfo.filmOverview = element.overview;
+            movieInfo.posterOfFilm = element.poster_path;
+            movieInfo.filmName = element.original_title;
+            movieInfo.filmOverview = element.overview;
 
-                arrayWithCards.push(createMovieCard(movieInfo));
-            });
+            arrayWithCards.push(createMovieCard(movieInfo));
+        });
 
-            const mainClassName = 'main--movie-list';
-            renderPageLayout(arrayWithCards, mainClassName);
+        const mainClassName = 'main--movie-list';
+        renderPageLayout(arrayWithCards, mainClassName);
 
-        } else {
-            onRejected();
-        }
-    };
-
-    function onRejected() {
-        console.log('error');
+    } else {
+        onRejected(result.status_message);
     }
+}
+
+function onRejected(error) {
+    const errorDiv = document.createElement('div');
+
+    document.body.prepend(errorDiv);
+    errorDiv.textContent = `Error: ${error}`;
+    errorDiv.className = 'error-div';
 }
